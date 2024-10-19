477f1d1a2e8d88054bc5b0d6a661d1a2c72654bb
Ajo Jose <151668867+ajojose803@users.noreply.github.com>
2024-10-19 17:55:06 +0530

    Time: 0 ms (100.00%), Space: 51.1 MB (9.19%) - LeetHub

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} original
+ * @return {number}
+ */
+var findFinalValue = function (nums, original) {
+    let value = original;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums.includes(value)) {
+            value *= 2;
+        } else {
+            return value;
+        }
+    }
+
+    return value;
+
+};
