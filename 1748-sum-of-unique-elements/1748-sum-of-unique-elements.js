540a81d063af05724c752a7b8fc4732ffbcc3d7d
Ajo Jose <151668867+ajojose803@users.noreply.github.com>
2024-10-19 18:04:04 +0530

    Time: 0 ms (100.00%), Space: 49.1 MB (61.43%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfUnique = function (nums) {
+    let arr = nums.filter((v) => nums.indexOf(v) === nums.lastIndexOf(v))
+        .reduce((sum, cur) => sum + cur, 0);
+
+    return arr;
+};
