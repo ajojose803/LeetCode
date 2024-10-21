dbd3ef9f27414937481aad6ec81f3ebc46625b86
Ajo Jose <151668867+ajojose803@users.noreply.github.com>
2024-10-21 18:59:42 +0530

    Time: 0 ms (100.00%), Space: 49.1 MB (35.64%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function (word1, word2) {
+    return word2.join('') == word1.join('')
+};
