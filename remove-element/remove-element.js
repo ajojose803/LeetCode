a59a6ecacf20e1091bdc12062416f8cfa931a282
Ajo Jose <151668867+ajojose803@users.noreply.github.com>
2024-10-30 18:17:23 +0530

    Time: 8 ms, Memory: 51.2 MB - LeetHub

---
+++ remove-element/remove-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    let k = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    nums.length = k; // Truncate the array
+    console.log("nums: ", nums, "nums length: ", k);
+    return k;
+};
